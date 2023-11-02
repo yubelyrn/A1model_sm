b0aca5ce5c5ab47e23dc310c8701a85df99640f9
Scott McElroy <scottmcelroy54@gmail.com>
2023-11-02 13:43:46 -0400

    Updating experimental data analysis documentation

--- batch_tu_mpi_ASSR.py
+++ batch_tu_mpi_ASSR.py
@@ -56,18 +56,18 @@ def assr_batch(filename):
     initCfg['recordDipole'] = True
     initCfg['saveCellSecs'] = False
     initCfg['saveCellConns'] = False
-    
+
     # from prev - best of 50% cell density
     updateParams = ['EEGain', 'EIGain', 'IEGain', 'IIGain',
                     ('EICellTypeGain', 'PV'), ('EICellTypeGain', 'SOM'), ('EICellTypeGain', 'VIP'), ('EICellTypeGain', 'NGF'),
                     ('IECellTypeGain', 'PV'), ('IECellTypeGain', 'SOM'), ('IECellTypeGain', 'VIP'), ('IECellTypeGain', 'NGF'),
                     ('EILayerGain', '1'), ('IILayerGain', '1'),
-                    ('EELayerGain', '2'), ('EILayerGain', '2'),  ('IELayerGain', '2'), ('IILayerGain', '2'), 
-                    ('EELayerGain', '3'), ('EILayerGain', '3'), ('IELayerGain', '3'), ('IILayerGain', '3'), 
-                    ('EELayerGain', '4'), ('EILayerGain', '4'), ('IELayerGain', '4'), ('IILayerGain', '4'), 
-                    ('EELayerGain', '5A'), ('EILayerGain', '5A'), ('IELayerGain', '5A'), ('IILayerGain', '5A'), 
-                    ('EELayerGain', '5B'), ('EILayerGain', '5B'), ('IELayerGain', '5B'), ('IILayerGain', '5B'), 
-                    ('EELayerGain', '6'), ('EILayerGain', '6'), ('IELayerGain', '6'), ('IILayerGain', '6')] 
+                    ('EELayerGain', '2'), ('EILayerGain', '2'),  ('IELayerGain', '2'), ('IILayerGain', '2'),
+                    ('EELayerGain', '3'), ('EILayerGain', '3'), ('IELayerGain', '3'), ('IILayerGain', '3'),
+                    ('EELayerGain', '4'), ('EILayerGain', '4'), ('IELayerGain', '4'), ('IILayerGain', '4'),
+                    ('EELayerGain', '5A'), ('EILayerGain', '5A'), ('IELayerGain', '5A'), ('IILayerGain', '5A'),
+                    ('EELayerGain', '5B'), ('EILayerGain', '5B'), ('IELayerGain', '5B'), ('IILayerGain', '5B'),
+                    ('EELayerGain', '6'), ('EILayerGain', '6'), ('IELayerGain', '6'), ('IILayerGain', '6')]
 
     for p in updateParams:
         if isinstance(p, tuple):
@@ -75,7 +75,7 @@ def assr_batch(filename):
         else:
             initCfg.update({p: cfgLoad[p]})
 
-    # good thal params for 100% cell density 
+    # good thal params for 100% cell density
     updateParams2 = ['thalamoCorticalGain', 'intraThalamicGain', 'EbkgThalamicGain', 'IbkgThalamicGain', 'wmat']
 
     for p in updateParams2:
@@ -95,11 +95,11 @@ def assr_batch(filename):
     def fitnessFunc(simData, **kwargs):
         import numpy as np
         from scipy import signal as ss
-        
+
         fs = 10000
         nperseg = int(fs/2)
         #s = int(1.75*fs)
-        s = int(0) 
+        s = int(0)
 
         electrodes = [3,4,5,6,7,8,9,10,11,12]
         powers = np.zeros((len(electrodes),))
@@ -109,7 +109,7 @@ def assr_batch(filename):
             lfp = lfps[s:,e]
             freq_wel, ps_wel = ss.welch(lfp,fs=fs,nperseg=nperseg)
             powers[i] = np.sum(ps_wel[38:42])
-        
+
         fitness = (10**4)*np.mean(powers)
 
         info = '; '.join(['power=%.6f fit=%.2f' % (p, fitness) for p in powers])
@@ -119,9 +119,9 @@ def assr_batch(filename):
 
 
     b = Batch(params=params, netParamsFile='netParams_ASSR.py', cfgFile='cfg_ASSR.py', initCfg=initCfg, groupedParams=groupedParams)
-    
+
     b.method = 'optuna'
-    
+
     b.optimCfg = {
         'fitnessFunc': fitnessFunc, # fitness expression (should read simData)
         'fitnessFuncArgs': fitnessFuncArgs,
@@ -130,7 +130,7 @@ def assr_batch(filename):
         'maxtime':      None,    #    Maximum time allowed, in seconds
         'maxiter_wait': 500,
         'time_sleep': 150,
-        'popsize': 1  # unused - run with mpi 
+        'popsize': 1  # unused - run with mpi
     }
 
     return b
@@ -143,7 +143,7 @@ def assr_batch_grid(filename):
     if not filename:
         filename = 'data/v34_batch25/trial_2142/trial_2142_cfg.json'
 
-    # from prev 
+    # from prev
     import json
     with open(filename, 'rb') as f:
         cfgLoad = json.load(f)['simConfig']
@@ -250,6 +250,11 @@ if __name__ == '__main__':
 
     setRunCfg(b, 'hpc_sge')
     b.run() # run batch
+
+
+
+
+
 
 
     
