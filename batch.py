5b0e921f519a1f44eeecb79b4abf20baaa35222b
Scott McElroy <scottmcelroy54@gmail.com>
2023-11-07 10:18:53 -0500

    Updating experimental data analysis documentation

--- batch.py
+++ batch.py
@@ -27,9 +27,9 @@ def assr_batch_grid(filename):
     cfgLoad2 = cfgLoad
 
     # #### SET weights####
-    params[('thalL4E')] = [1.0, 1.5]
-    params[('ICThalweightEcore')] = [1.0, 1.5]
-    params[('ICThalweightIcore')] = [0.5, 1.0]
+    params[('thalL4E')] = [1.5]
+    params[('ICThalweightEcore')] = [1.5]
+    params[('ICThalweightIcore')] = [1.0]
 
     #### GROUPED PARAMS ####
     groupedParams = []
