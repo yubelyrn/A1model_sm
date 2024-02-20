7f72835a85b94e98070fc2b2889d6f6f8366bbdd
Scott McElroy <scottmcelroy54@gmail.com>
2024-02-20 12:12:02 -0500

    Slight naming tweaks before run

--- submit.sh
+++ submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #$ -cwd
-#$ -N ASSR_sim02_16
+#$ -N ASSR_cfgTune
 #$ -q cpu.q
 #$ -pe smp 60
 #$ -l h_vmem=256G
-#$ -l h_rt=3:00:00
-#$ -o /ddn/smcelroy97/A1model_sm/data/control_test.out
-#$ -e /ddn/smcelroy97/A1model_sm/data/control_test.err
+#$ -l h_rt=1:15:00
+#$ -o /ddn/smcelroy97/A1model_sm/data/singleSim.out
+#$ -e /ddn/smcelroy97/A1model_sm/data/singleSim.err
 
 source ~/.bashrc
 mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi init.py
