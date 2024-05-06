825f6ce7fd1fb3f4f7f435c9720bf941c9415b68
Scott McElroy <scottmcelroy54@gmail.com>
2024-05-06 09:28:25 -0400

    updating labels for parallel

--- submit.sh
+++ submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #$ -cwd
-#$ -N ASSR_cfgTune
+#$ -N ASSR_CochTune
 #$ -q cpu.q
 #$ -pe smp 30
 #$ -l h_vmem=256G
 #$ -l h_rt=2:00:00
-#$ -o /ddn/smcelroy97/A1model_sm/data/singleSim.out
-#$ -e /ddn/smcelroy97/A1model_sm/data/singleSim.err
+#$ -o /ddn/smcelroy97/A1model_sm/data/singleSim2.out
+#$ -e /ddn/smcelroy97/A1model_sm/data/singleSim2.err
 
 source ~/.bashrc
 mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi init.py
