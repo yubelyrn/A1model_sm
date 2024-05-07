c513ff2abbab55c64bd1da63974c64403026a54f
Scott McElroy <scottmcelroy54@gmail.com>
2024-05-07 12:47:02 -0400

    Taking Sam's sim.py

--- submit.sh
+++ submit.sh
@@ -4,9 +4,9 @@
 #$ -q cpu.q
 #$ -pe smp 30
 #$ -l h_vmem=256G
-#$ -l h_rt=2:00:00
+#$ -l h_rt=4:00:00
 #$ -o /ddn/smcelroy97/A1model_sm/data/singleSim.out
 #$ -e /ddn/smcelroy97/A1model_sm/data/singleSim.err
 
 source ~/.bashrc
-mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi init.py
+mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi sim.py
