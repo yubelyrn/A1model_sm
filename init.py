868a992032a273322f8c5cd5717e54a4cc834137
Scott McElroy <scottmcelroy54@gmail.com>
2024-05-07 10:36:40 -0400

    Updating experimental data analysis documentation

--- init.py
+++ init.py
@@ -95,18 +95,18 @@ def checkCochConns():
 
 checkCochConns()
 
-# sim.net.addStims() 							# add network stimulation
-# sim.setupRecording()              			# setup variables to record for each cell (spikes, V traces, etc)
-# sim.runSim()                      			# run parallel Neuron simulation
-# sim.gatherData()                  			# gather spiking data and cell info from each node
-#
-# # distributed saving (to avoid errors with large output data)
-# sim.saveDataInNodes()
-# sim.gatherDataFromFiles()
-# sim.saveData()
-# sim.analysis.plotData()    # plot spike raster etc
-#
-# now = datetime.now()
-#
-# current_time = now.strftime("%H:%M:%S")
-# print("Current Time =", current_time)
+sim.net.addStims() 							# add network stimulation
+sim.setupRecording()              			# setup variables to record for each cell (spikes, V traces, etc)
+sim.runSim()                      			# run parallel Neuron simulation
+sim.gatherData()                  			# gather spiking data and cell info from each node
+
+# distributed saving (to avoid errors with large output data)
+sim.saveDataInNodes()
+sim.gatherDataFromFiles()
+sim.saveData()
+sim.analysis.plotData()    # plot spike raster etc
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
