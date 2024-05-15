f2419e4b003e684d8886b72070c0d4c3911cbdcf
Scott McElroy <scottmcelroy54@gmail.com>
2024-05-15 15:57:38 -0400

    updating batchlabel

--- batch.py
+++ batch.py
@@ -132,7 +132,7 @@ if __name__ == '__main__':
     #b = assr_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'CochInputTune0515'
+    b.batchLabel = 'CochInputTune0515_2'
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_sge')
