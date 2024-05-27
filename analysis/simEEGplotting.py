436c794238b3fafaabd24026ee3f1eeef84435bb
Scott McElroy <scottmcelroy54@gmail.com>
2024-05-27 12:57:08 -0400

    Checking out traces

--- analysis/simEEGplotting.py
+++ analysis/simEEGplotting.py
@@ -1,31 +1,137 @@
 from netpyne import sim
+from netpyne.plotting import plotCSDPSD
+from netpyne.support.morlet import MorletSpec, index2ms
+from netpyne.analysis import spikes_legacy
 from simTools import simTools
 import os
 import numpy as np
-# snippet of code to import matplotlib and dynamically switch backend to "MacOSX" for plotting
-from pydoc import source_synopsis
-import sys
-from netpyne.support.morlet import MorletSpec, index2ms
+from scipy import signal
 import matplotlib
+
 matplotlib.use("MacOSX")
 from matplotlib import pyplot as plt
 from lfpykit.eegmegcalc import NYHeadModel
 
+batch = 'ASSR_tune_0228'  # Name of batch for fig saving
 
-batch = 'ASSR_grid_0310' #Name of batch for fig saving
+stim_on = 3000
+# calcEEG = {'start': 2800, 'stop': 4000}
+# filter = {'lowCut':2, 'hiCut': 12}
+# plotERP = {'useFilter': True}
+# plotSpectrogram = {'useFilter': True}
+# plotPSD = {'useFilter': True}
+# plotRaster = {'timeRange': [2800, 4000]}
+PSDSpect = {'timeRange': [3000, 4000], 'useLFP': False, 'useCSD': True}
+
+calcEEG = False
+filter = False
+plotERP = False
+plotSpectrogram = False
+plotPSD = False
+plotRaster = False
+# PSDSpect = False
 
 # Load sim EEG data
-base_dir = '/Users/scottmcelroy/A1_scz/A1_sim_data/'+ batch +'/'
+base_dir = '/Users/scottmcelroy/A1_scz/A1_sim_data/' + batch + '/'
 for file in os.listdir(base_dir):
     if file.endswith('.pkl'):
-        sim.load(os.path.join(base_dir, file), 'rb', instantiateStims=False, instantiateConns=False)
-        fname = file[0:18]
-        stim_data, stim_window, eeg = simTools.calculateEEG(sim, stimOn=4000, end=5000)
-        # filtered_data = simTools.filterEEG(stim_data, 1, 80, 1000, 4)
-        # simTools.plotERP(filtered_data, stim_window, fname, batch)
-        # simTools.plot_spectrogram(data=filtered_data, time=stim_window, fname=fname, batch=batch)
+        sim.initialize()
+        all = sim.loadAll(os.path.join(base_dir, file))
+        fname = file[0:-9]  # Create filename (can change to whatever)
+        if not os.path.exists('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch):
+            os.mkdir( '/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch)  # Create Figure directory if one doesn't already exist
+
+        # Calculate EEG signal at one electode (currently set to 'Cz'
+        if calcEEG:
+            stim_data, stim_window = simTools.calculateEEG(
+                sim,
+                start=2800,
+                end=3500)  # Generate EEG data from dipole sums
+            # Time vector starting at t=0 instead of timeRange[0]
+            offsetStart = calcEEG['start'] - stim_on
+            endWindow = calcEEG['end'] - stim_on
+            t = np.arange(offsetStart, endWindow, 0.05)
+
+        # Filter EEG data
+        if filter:
+            filtered_data = simTools.filterEEG(
+                stim_data,
+                lowcut=['lowCut'],
+                highcut=filter['hiCut'],
+                fs=20000,
+                order=2)  # Filter (if needed)
+
+        # Plot ERP '
+        if plotERP:
+            if plotERP['useFilter'] == True:
+                simTools.plotERP(filtered_data, t, fname, batch)  # Create ERP plot of time window specified
+
+            else:
+                simTools.plotERP(stim_data, t, fname, batch)
+
+        # Plot EEG Spectrogram
+        if plotSpectrogram:
+            if plotSpectrogram['useFilter'] == True:
+                simTools.plot_spectrogram(
+                    data=filtered_data,
+                    time=stim_window,
+                    fname=fname,
+                    batch=batch)
+
+            else:
+                simTools.plot_spectrogram(
+                    data=filtered_data,
+                    time=stim_window,
+                    fname=fname,
+                    batch=batch)
+
+        # Plot EEG PSD
+        if plotPSD:
+            if plotPSD['useFilter'] == True:
+                simTools.plot_PSD(
+                    data=filtered_data,
+                    time=stim_window,
+                    fname=fname,
+                    batch=batch)
+
+            else:
+                simTools.plot_spectrogram(
+                    data=stim_data,
+                    time=stim_window,
+                    fname=fname,
+                    batch=batch)
+
+        # Plot Raster
+        if plotRaster:
+            sim.analysis.plotRaster(
+                orderInverse=True,
+                timeRange=plotRaster['timeRange'],
+                markerSize=1000,
+                figSize=(27, 23),
+                saveFig='/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'
+                        + batch + '/' + fname + 'Raster.png')
+
+        if PSDSpect:
+            simTools.plotPSDSpectrogram(
+                    sim,
+                    batch,
+                    fname,
+                    timeRange = PSDSpect['timeRange'],
+                    useLFP=PSDSpect['useLFP'],
+                    useCSD=PSDSpect['useCSD'])
+
+# # Plot LFP PSD
+#         sim.analysis.plotLFP(plots = 'PSD', saveFig= '/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'+batch+ '/'+fname+ 'LFP.png')
+# # Plot CSD
+#         sim.plotting.plotCSD(overlay= 'CSD', timeRange=[2500, 5000],saveFig='/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch+'/'+fname+'CSDpad.jpeg')
+#         spikes_legacy.plotRatePSD(include = ['IT2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'CT5A', 'IT5B', 'CT5B' , 'PT5B', 'IT6', 'CT6'],timeRange=[3000, 5000],  saveFig='/Users/scottmcelroy/Desktop/ASSRratePSDAll.png')
+# spikes_legacy.plotRatePSD(include = ['ITP4', 'ITS4'], timeRange=[3000, 5000], saveFig='/Users/scottmcelroy/Desktop/ASSRratePSDGran.png')
+# spikes_legacy.plotRatePSD(include = ['IT5A', 'CT5A'], timeRange=[3000, 5000], saveFig='/Users/scottmcelroy/Desktop/ASSRratePSDL5A.png')
+# spikes_legacy.plotRatePSD(include = ['IT5B', 'CT5B' , 'PT5B'], timeRange=[3000, 5000], saveFig='/Users/scottmcelroy/Desktop/ASSRratePSDL5B.png')
+# spikes_legacy.plotRatePSD(include = ['IT6', 'CT6'], timeRange=[3000, 5000], saveFig='/Users/scottmcelroy/Desktop/ASSRratePSDL6.png')
 
 
-# Line ran for grant fig raster
-# sim.analysis.plotRaster(timeRange=(4000,5000), orderInverse=True, markerSize=1000, figSize = (27,23),
-# saveFig = '/Users/scottmcelroy/A1_scz/A1_figs/0228_RasterF')
+################## Scrap for resampling if needed later ##################################################
+# num_samples = len(stim_data)
+# new_num_samples = int(num_samples * 1000/ 20000)
+# resampled_data = signal.resample(stim_data, new_num_samples)
--- analysis/simTools.py
+++ analysis/simTools.py
@@ -5,20 +5,21 @@ from netpyne.support.morlet import MorletSpec, index2ms
 import os
 import numpy as np
 import matplotlib
-matplotlib.use("MacOSX")
+import random
 from    matplotlib  import  pyplot  as plt
 
-
-
+#####################################################
+#    Funcitons for analysis, code to use found in simEEGplotting.py     #
+#####################################################
 class simTools:
-    def calculateEEG(sim, stimOn, end):
+    def calculateEEG(sim, start, end):
         # Load 3D head model for EEG
         nyhead = NYHeadModel(nyhead_file=os.getenv('NP_LFPYKIT_HEAD_FILE', None))
-        nyhead.set_dipole_pos('parietal_lobe')
+        nyhead.set_dipole_pos([ 39.74573803, -21.57684261, 7.82510972])
         M = nyhead.get_transformation_matrix()
 
         # Adjsut time for stimulation window
-        timeRange = [stimOn, end]
+        timeRange = [start, end]
         timeSteps = [int(timeRange[0] / 0.05), int(timeRange[1] / 0.05)]
         t = np.arange(timeRange[0], timeRange[1], 0.05)
 
@@ -30,14 +31,18 @@ class simTools:
 
         # Calculate EEG
         eeg = M @ p * 1e9
-        goodchan = eeg[38, :]
+        goodchan = eeg[48, :]
+        # goodchan = eeg[38]
 
-        onset = int(stimOn / 0.05)
+        onset = int(start / 0.05)
         offset = int(end / 0.05)
         return goodchan, t
 
     def filterEEG(EEG, lowcut, highcut, fs, order):
-        b, a = butter(order, [lowcut, highcut], btype='band', fs=fs)
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, [low, high], btype='band')
         filtered_signal = filtfilt(b, a, EEG)
         return filtered_signal
     def processRaster(spkTimes, spkGids):
@@ -61,15 +66,55 @@ class simTools:
         return spk_gids, spk_times
     def plotERP(data, time, fname, batch, figsize = (30,20)):
         plt.figure(figsize=figsize)
-        plt.plot(time,data/1000, linewidth = 4)
+        plt.plot(time,data/1000, color='black', linewidth = 8)
+        plt.axhline(y=0, color='black',linestyle='-', linewidth = 4)
         plt.tick_params(labelsize=50)
-        plt.xlabel('Time (s)', fontsize = 65)
+        plt.xlabel('Time (ms)', fontsize = 65)
         plt.ylabel('uV', fontsize = 65)
-        if not os.path.exists('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch):
-            os.mkdir('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch)
-        plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch + '/' + fname + 'ERP.png')
+        plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'
+                    + batch + '/' + fname + 'ERPnew.png')
+        print('saved')
 
     def plot_spectrogram(data, time, fname, batch, figsize = (20,20)):
+        # sampling frequency
+        fs = int(1000.0 / 0.05)
+
+        # Define freqs you care about
+        minFreq = 1
+        maxFreq = 80
+
+        # Perform Morlet transform
+        freqList = None
+        spec = (MorletSpec(data, fs, freqmin=minFreq, freqmax=maxFreq, freqstep=1, lfreq=freqList))
+
+        # Min and mox for the color of normalized power
+        vmin = spec.TFR.min()
+        vmax = spec.TFR.max()
+
+        T = time  # Time
+        F = spec.f  # Define frequencies
+        S = spec.TFR  # Spectral data for spectrogram
+
+        # Spectrogram plot params
+        plt.figure(figsize=figsize)
+        plt.tick_params(labelsize=50)
+        plt.xlabel('Time (ms)', fontsize = 65)
+        plt.ylabel('Frequency (Hz)', fontsize = 65)
+        plt.imshow(
+            S,
+            extent=(np.amin(T), np.amax(T), np.amin(F), np.amax(F)),
+            origin='lower',
+            interpolation='None',
+            aspect='auto',
+            vmin=vmin,
+            vmax=vmax,
+            cmap=plt.get_cmap('viridis')
+            )
+        plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'
+                    + batch + '/' + fname + 'spect.png')
+
+
+    def plot_PSD(data, time, fname, batch, figsize = (20,20)):
         # sampling frequency
         fs = int(1000.0 / 0.05)
 
@@ -90,24 +135,132 @@ class simTools:
         S = spec.TFR  # Spectral data for spectrogram
         signal = 10 * (np.log10(np.mean(S, 1)))  # Use this for PSD plotting
 
-        # Spectrogram plot params
+        # PSD plot params
         plt.figure(figsize=figsize)
         plt.xlabel('Time (s)')
-        plt.ylabel('Frequency (Hz)')
-        plt.imshow(S, extent=(np.amin(T), np.amax(T), np.amin(F), np.amax(F)),
-                   origin='lower',
-                   interpolation='None',
-                   aspect='auto',
-                   vmin=vmin,
-                   vmax=vmax,
-                   cmap=plt.get_cmap('viridis')
-                   )
-        if not os.path.exists('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch):
-            os.mkdir('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch)
-        plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/' + batch + '/' + fname + 'spect.png')
+        plt.ylabel('Power')
+        plt.plot(F, signal)
+        plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'
+                    + batch + '/' + fname + 'PSD.png')
+
+
+    def plotPSDSpectrogram(
+            sim,
+            batch,
+            fname,
+            timeRange,
+            showMin = 1,
+            showMax = 100,
+            filtFreq= False,
+            useLFP = True,
+            useCSD = True):
+
+        if useCSD and useLFP:
+            lfp_PSD = sim.analysis.preparePSD(
+                CSD=False,
+                minFreq=showMin,
+                maxFreq=showMax,
+                filtFreq=filtFreq,
+                timeRange=timeRange)
+            csd_PSD = sim.analysis.preparePSD(
+                CSD=True,
+                minFreq=showMin,
+                maxFreq=showMax,
+                filtFreq=filtFreq,
+                timeRange=timeRange)
+
+            plt.figure(figsize=(12, 6))
+            plt.subplot(121)
+            plt.imshow(lfp_PSD['psdSignal'], aspect='auto', origin='lower', cmap='viridis')
+            plt.gca().invert_yaxis()
+            plt.colorbar(label='LFP Amplitude')
+            plt.xlim([20, 100])
+            plt.xlabel('Frequency')
+            plt.ylabel('Electrode Depth')
+            plt.title('Local Field Potential (LFP) PSD Spectrogram')
+
+            # Plotting the CSD heatmap
+            plt.subplot(122)
+            plt.imshow(csd_PSD['psdSignal'], aspect='auto', origin='lower', cmap='viridis')
+            plt.gca().invert_yaxis()
+            plt.colorbar(label='CSD Amplitude')
+            plt.xlim([20, 100])
+            plt.xlabel('Frequency')
+            plt.ylabel('Electrode Depth')
+            plt.title('Current Source Density (CSD) PSD Spectrogram')
+
+            plt.tight_layout()
+            plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'
+                        + batch + '/' + fname + 'PSDspect.png')
+        elif useLFP and not useCSD:
+            lfp_PSD = sim.analysis.preparePSD(
+                CSD=False,
+                minFreq=showMin,
+                maxFreq=showMax,
+                filtFreq=filtFreq,
+                timeRange=timeRange)
+
+            plt.imshow(lfp_PSD['psdSignal'], aspect='auto', origin='lower', cmap='viridis')
+            plt.gca().invert_yaxis()
+            plt.colorbar(label='LFP Amplitude')
+            plt.xlim([20, 100])
+            plt.xlabel('Frequency')
+            plt.ylabel('Electrode Depth')
+            plt.title('Local Field Potential (LFP) PSD Spectrogram')
+            plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'
+                        + batch + '/' + fname + 'LFPPSDspect.png')
+        else:
+            csd_PSD = sim.analysis.preparePSD(
+                CSD=True,
+                minFreq=showMin,
+                maxFreq=showMax,
+                filtFreq=filtFreq,
+                timeRange=timeRange)
+
+            plt.imshow(csd_PSD['psdSignal'], aspect='auto', origin='lower', cmap='viridis')
+            plt.gca().invert_yaxis()
+            plt.colorbar(label='CSD Amplitude')
+            plt.xlim([20, 100])
+            plt.xlabel('Frequency')
+            plt.ylabel('Electrode Depth')
+            plt.title('Current Source Density (CSD) PSD Spectrogram')
+
+            plt.tight_layout()
+            plt.savefig('/Users/scottmcelroy/A1_scz/A1_figs/SIMfigs/'
+                        + batch + '/' + fname + 'CSDPSDspect.png')
+
+#####################################################
+#    Funcitons for editing a network after cells and conns are made     #
+#####################################################
+class editNet:
+
+    def pruneSynapses(cell, conn, probability, pruning_range):
+        # Get the section
+        sec = cell.secs[conn['sec']]
+
+        # Get the 3D points of the section
+        points = sec['geom']['pt3d']
+
+        y1 = points[0][1]
+        y2 = points[1][1]
+
+        syn_rel = conn['loc'] * (y2 - y1)
+
+        # Get the position of the cell within the network
+        y_cell = cell.tags['y']
+
+        # Calculate the 3D coordinates relative to the network
+        y_net = syn_rel + y_cell
+        if pruning_range[1] > y_net > pruning_range[0]:
+            if random.random() < probability:
+                cell.conns.remove(conn)
 
 
 
 
 
 
+################## Scrap for resampling if needed later ##################################################
+# num_samples = len(stim_data)
+# new_num_samples = int(num_samples * 1000/ 20000)
+# resampled_data = signal.resample(stim_data, new_num_samples)
