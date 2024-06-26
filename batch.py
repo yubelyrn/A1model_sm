81796c1daea13b9bec0ef71665420b1acee65d4b
yubelyrn <yubelyrendon@gmail.com>
2024-06-26 14:50:24 -0500

    new value

--- batch.py
+++ batch.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
     #b = assr_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'ThalL4E0626_09'
+    b.batchLabel = 'ThalL4E0626_9'
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_sge')
