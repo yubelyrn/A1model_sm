be3a4386454fac747d9439d7425b2146c0e818e4
yubelyrn <yubelyrendon@gmail.com>
2024-08-13 20:59:58 -0500

    testing EI with layer 4

--- batch.py
+++ batch.py
@@ -40,7 +40,8 @@ def assr_batch_grid(filename):
     params['thalL4E'] = [2.5]
     params['thalIIScale'] = [1.75]
     params['EEGain'] = [0.3]
-    params['EELayerGain', '4'] = [0.2, 0.4, 0.6]
+    #params['EELayerGain', '4'] = [0.2, 0.4, 0.6]
+    params['EILayerGain', '4'] = [0.5, 0.7, 0.9]
     params['EELayerGain', '5A'] = [3.0, 3.5, 3.8]
     params['EELayerGain', '5B'] = [1.5, 2.0, 2.5]
     params['EELayerGain', '6'] = [1.8, 1.9, 2.1]
@@ -148,7 +149,7 @@ if __name__ == '__main__':
     #b = assr_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'ThalL4E_IIScale_EEGain_LayerGain__4_5A_5B_6_08_13'
+    b.batchLabel = 'ThalL4E_IIScale_EEGain_LayerGain__4_5A_5B_6_08_13_2'
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_sge')
